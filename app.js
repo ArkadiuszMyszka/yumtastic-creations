75e5e30d7e4ae3d9506044232ade1b33cc41d454
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2024-02-14 23:18:07 +0100

    connecting with db

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-console.log("app działa")
+console.log('app działa');
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import process from 'node:process';
 import { app } from './app.js';
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 3001;
+const connection = mongoose.connect(process.env.DB_HOST);
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`);
+// });
+
+const startServer = async () => {
+  try {
+    await connection;
+    console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  } catch (err) {
+    console.log('db not connected');
+    process.exit(1);
+  }
+};
+
+startServer();
