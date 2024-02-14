fb68b4c57beeeb5f4392e31aed278d951807a659
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-14 06:06:00 +0100

    .posthtmlrc fixed

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ import process from 'node:process';
 import { app } from './app.js';
 import 'dotenv/config';
 
-
 const PORT = process.env.PORT || 3001;
 
-
-
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
-    })
-
+app.listen(PORT, () => {
+  console.log(`Server running. Use our API on port: ${PORT}`);
+});
