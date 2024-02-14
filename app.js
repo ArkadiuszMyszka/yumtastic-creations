340fca97ac11193ccb940e53abd5ebcb1f7577a5
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-14 05:02:09 +0100

    Configurations + some  backend packages

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+console.log("app działa")
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+export { app };
---
+++ server.js
@@ -0,0 +1,13 @@
+import process from 'node:process';
+import { app } from './app.js';
+import 'dotenv/config';
+
+
+const PORT = process.env.PORT || 3001;
+
+
+
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    })
+
