9dc03774f39139c2a4f62079818e06c01ed89e86
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-19 00:02:33 +0100

    done

--- server/routes/recipesRoutes/idRecipeRoutes.js
+++ server/routes/recipesRoutes/idRecipeRoutes.js
@@ -1,5 +1,14 @@
+import { Recipe } from "../../service/schemas/recipes.js";
+
 async function idRecipe(req, res, next) {
-  return res.json(req.params.id).status(200);
+  const recipeID = req.params.id;
+  try {
+    const findRecipe = await Recipe.findById(recipeID);
+    return res.json(findRecipe).status(200);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json(e);
+  }
 }
 
 export { idRecipe };
