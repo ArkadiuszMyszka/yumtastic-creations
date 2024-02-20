b4407020acb3041a583eb88dc6be612c5089ca52
Bartosz Potyrała <potyrala.bartosz@gmail.com>
2024-02-20 01:53:23 +0100

    adding @patch function to add recipe to favorite array in db

--- server/routes/favoritesRoutes.js
+++ server/routes/favoritesRoutes.js
@@ -2,9 +2,11 @@ import { Router } from "express";
 // import { User } from "#models/User.js";
 import { authMiddleware } from "#auth/authMiddleware.js";
 import { favoritesList } from "./favoritesRoutes/favoritesListRoutes.js";
+import { patchFavorites } from "./favoritesRoutes/addToFavoritesRoutes.js";
 
 const favorites = Router();
 
 favorites.get("/favorites", authMiddleware, favoritesList);
+favorites.patch("/favorites", authMiddleware, patchFavorites);
 
 export default favorites;
