6f4ba2c069cef359f605212d2169461105e25929
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-21 00:38:59 +0100

    Prepare

--- server/routes/recipesRoutes.js
+++ server/routes/recipesRoutes.js
@@ -12,7 +12,51 @@ import searchRecipe from './recipesRoutes/searchRecipeRoutes.js';
 
 const recipes = Router();
 
+/**
+ * @openapi
+ * tags:
+ *  name: Recipes
+ *  description: Recipes routes to manage recipes
+ */
+
+/**
+ * @openapi
+ * /recipes:
+ *  get:
+ *    summary: Allows user and applicaion to get all recipes from db
+ *    tags: [Recipes]
+ *    responses:
+ *      200:
+ *        description: Data loaded successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                  $ref: "#/components/schemas/Recipe"
+ */
+
 recipes.get("/recipes/category-list", authMiddleware, categoryList);
+
+/**
+ * @openapi
+ * /recipes/category-list:
+ *  get:
+ *    summary: Allows application to get a last of recipes categories
+ *    tags: [Recipes]
+ *    responses:
+ *      200:
+ *        description: Lisr of categories loaded successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: string
+ *              example: ["Beef", "Pork"]
+ */
+
+
 recipes.get("/recipes/main-page", authMiddleware, categoryMainPage);
 recipes.get('/recipes/:id', authMiddleware, idRecipe);
 recipes.get("/recipes/:category", authMiddleware, categoryRecipes);
