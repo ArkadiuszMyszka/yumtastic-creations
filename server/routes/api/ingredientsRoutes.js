6b196dcee6d678c249f0614f169eb88e2bd5f251
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-21 22:43:36 +0100

    list ready

--- server/routes/api/ingredientsRoutes.js
+++ server/routes/api/ingredientsRoutes.js
@@ -5,7 +5,36 @@ import ingredientsList from "../../controllers/ingredientsRoutes/ingredientsList
 import recipesByIngredients from "#controllers/ingredientsRoutes/recipesByController.js";
 const ingredients = Router();
 
+/**
+ * @openapi
+ * tags:
+ *  name: Ingredients
+ *  description: Brings stored ingredients
+ */
+
+/**
+ * @openapi
+ * /ingredients/list:
+ *   get:
+ *      tags: [Ingredients]
+ *      summary: Returns a list of available ingredients
+ *      responses:
+ *       200:
+ *          description: Array of ingredients
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/ingredient"
+ *       401:
+ *          description: User is not authorized
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#components/schemas/Error"
+ */
+
 ingredients.get("/list", authMiddleware, ingredientsList);
+
 ingredients.get("/", authMiddleware, recipesByIngredients);
 
 export default ingredients;
