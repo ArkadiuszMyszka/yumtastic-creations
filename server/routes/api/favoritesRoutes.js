3073b769e083b8e35b754301d5e140f2a731ef6d
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-21 20:56:52 +0100

    .

--- server/routes/api/favoritesRoutes.js
+++ server/routes/api/favoritesRoutes.js
@@ -5,6 +5,14 @@ import { patchFavorites } from "#controllers/favoritesRoutes/patchFavoritesContr
 
 const favorites = Router();
 
+/**
+ * @openapi
+ * tags:
+ *  name: Favorites
+ *  description: Allows users to manage favorite recipes, i.e. add to favorite or delete
+ */
+
+
 favorites.get("/favorites", authMiddleware, favoritesList);
 favorites.patch("/favorites", authMiddleware, patchFavorites);
 
