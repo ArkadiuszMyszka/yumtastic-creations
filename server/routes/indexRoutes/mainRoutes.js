3ee038b9d733a0f5c9bd59a8620f23c79e3d8319
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-02-22 21:29:35 +0100

    1

--- server/routes/indexRoutes/mainRoutes.js
+++ server/routes/indexRoutes/mainRoutes.js
@@ -1,7 +1,7 @@
 import { User } from '#models/User.js';
 
 // jak na razie na main dostajemy wszystkich userów
-// w przyszłości do usuniecia
+// w przyszłości do usuniecia.
 async function main(req, res, next) {
   try {
     const users = await User.find({}).lean();
