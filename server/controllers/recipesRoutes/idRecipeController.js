abbb2f4047aed4f2ff76425f083a819e89c2a537
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-02-23 07:05:23 +0100

    Fixes

--- server/controllers/recipesRoutes/idRecipeController.js
+++ server/controllers/recipesRoutes/idRecipeController.js
@@ -2,16 +2,21 @@ import { Recipe } from "#schemas/recipes.js";
 
 async function idRecipe(req, res, next) {
   const recipeID = req.params.id;
-  try {
-    const findRecipe = await Recipe.findById(recipeID);
-    if (!findRecipe) {
-      return res.status(404).json({ message: "Recipes not found" });
-    }
-    return res.json(findRecipe).status(200);
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json(e);
+  const recipeIdLength = recipeID.length
+  if (recipeIdLength === 24) {
+     try {
+       const findRecipe = await Recipe.findById(recipeID);
+       if (!findRecipe) {
+         return res.status(404).json({ message: "Recipes not found" });
+       }
+       return res.json(findRecipe).status(200);
+     } catch (e) {
+       console.log(e);
+       return res.status(500).json(e);
+     }
   }
+  return next()
+ 
 }
 
 export { idRecipe };
