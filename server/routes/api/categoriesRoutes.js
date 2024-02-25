e91818118af209d7413921aefc85c8a33af58197
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2024-02-25 20:03:12 +0100

    Create categoriesRoutes.js

---
+++ server/routes/api/categoriesRoutes.js
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { authMiddleware } from "#middlewares/authMiddleware.js";
+import { categoriesPage } from "#controllers/categoriesController.js"; // Import kontrolera dla strony kategorii
+
+const router = Router();
+
+/**
+ * @openapi
+ * tags:
+ *  name: Categories
+ *  description: Allows users to view categories of recipes
+ */
+/**
+ * @openapi
+ *  /categories:
+ *      get:
+ *          tags: [Categories]
+ *          summary: Returns collection of recipe categories
+ *          responses:
+ *              200:
+ *                  description: Categories found
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  categories:
+ *                                      type: array
+ *                                      items:
+ *                                          type: string
+ *              401:
+ *                 description: User is not authorized
+ *                 content:
+ *                    application/json:
+ *                        schema:
+ *                            $ref: "#components/schemas/Error"
+ */
+
+router.get("/categories", authMiddleware, categoriesPage); // Zaktualizowana trasa prowadząca do strony kategorii
+
+export default router;
