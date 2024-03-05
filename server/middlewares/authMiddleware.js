1b07ba74c49c7583f4e619e39d74c9c09f293591
Arkadiusz Myszka <lokonhinio@gmail.com>
2024-03-05 23:42:51 +0100

    logout controller created and authMiddleware updated

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,12 +1,19 @@
 import passport from 'passport';
 
 export const authMiddleware = (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, (error, user) => {
-    
+  const token = req.headers.authorization
+  
+  passport.authenticate("jwt", { session: false }, async (error, user) => {
     if (!user || error) {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: "Unauthorized" });
     }
     res.locals.user = user;
+    if (user.isLogged === false) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    if (token !== `Bearer ${user.token}`) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
     next();
   })(req, res, next);
 };
